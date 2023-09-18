84e276e9ae3e98d1733dbda143bedfa81d15bab1
Arjun Malhotra <hiarjun.malhotra2002@gmail.com>
2023-09-19 01:19:39 +0530

    update footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,21 @@
+import Link from 'next/link';
 import React from 'react';
+import { FaGithub } from 'react-icons/fa';
+import { FiMail } from 'react-icons/fi';
 
 const Footer = () => {
   return (
-    <div className="bg-gray-900 text-white py-4 text-center font-bold text-2xl">
-      Made by Arjun Malhotra
+    <div className="bg-gray-900 text-white py-4 text-center font-bold text-2xl flex justify-center items-center space-x-4">
+      <h3>
+        Made by Arjun Malhotra
+      </h3>
+      <Link href="https://github.com/ArjunMal1311/">
+        <FaGithub />
+      </Link>
+
+      <Link href="mailto:hiarjun.malhotra2002@gmail.com">
+        <FiMail />
+      </Link>
     </div>
   );
 };
